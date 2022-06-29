e0598348d7307cfcba690c528e9bc00941c2e0a4
vignesh <vigneshfornavy@gmail.com>
2022-06-29 19:31:13 +0530

    new logical

---
+++ Desktop/javaDev/day5.java
@@ -0,0 +1,13 @@
+public class day5{
+    public static void main(String[] args) {
+        int topScore =96;
+        if((topScore<80) && (topScore<10)){
+            System.out.println(" top score is greater....");
+        }
+        else if((topScore>97)||(topScore==96)){
+            System.out.println("one is scored 96 or above");
+        }else{
+        System.out.println("no one is scored expected marks..");
+        }
+    }
+}
