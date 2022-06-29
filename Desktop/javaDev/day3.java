2c5fae941c345511e2af69c789a408ef04f3ff78
vignesh <vigneshfornavy@gmail.com>
2022-06-29 19:16:14 +0530

    if else

---
+++ Desktop/javaDev/day3.java
@@ -0,0 +1,11 @@
+public class day3{
+    public static void main(String[] args) {
+        int min=Integer.MIN_VALUE;
+        int max=Integer.MAX_VALUE;
+        System.out.println(min);
+        System.out.println(max);
+        System.out.println("Busted min value is"+(min+1));
+        System.out.println("busted max value is "+(max+1));
+        int n=214748364;
+    }
+}
---
+++ Desktop/javaDev/day4.java
@@ -0,0 +1,17 @@
+public class day4 {
+    public static void main(String[] args) {
+        int result=1;
+        result++;
+        System.out.println(result);
+        result--;
+        System.out.println(result);
+        result +=2;
+        System.out.println(result);
+        result *=10;
+        System.out.println(result);
+        result /=3;
+        System.out.println(result);
+        result -=2;
+        System.out.println(result);
+    }
+}
