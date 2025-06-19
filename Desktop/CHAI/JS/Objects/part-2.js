7839d99429dc6ef0b9b5356ee91d26315e1fe0fd
VigneshWorkcrew <vignesh@workcrew.ai>
2025-06-19 18:08:01 +0530

    React series

---
+++ Desktop/CHAI/JS/Objects/part-2.js
@@ -0,0 +1,9 @@
+const student = {
+  name: "vignesh H E",
+  age: 21,
+  course: "B.Tech",
+};
+
+console.log(student.name);
+
+console.log(student.age);
---
+++ Desktop/CHAI/JS/Objects/part.js
@@ -0,0 +1,7 @@
+const student = {
+  name: "vignesh H E",
+  age: 21,
+  course: "B.Tech",
+};
+
+const copy = { ...student };
